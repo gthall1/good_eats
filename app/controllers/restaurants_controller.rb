7bf288beadfd88caad4d52dd79a493f52486736a
Griff Hall <griffhall1@gmail.com>
2014-06-21 21:03:12 -0400

    added reviews table. added index,show,new, and edit functionality for restaurants

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,32 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @new_restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :city, :state, :zipcode, :description, :category)
+  end
+end
+
+
+
+
+
+
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :restaurants
+
+  validates :rating, inclusion: (1..5), presence: true
+  validates :body, presence: true
 end
